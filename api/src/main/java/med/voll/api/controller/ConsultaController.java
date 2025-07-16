831ab2eec7c63dcc8aeb412780a3ae72e34294d0
Douglas Prado <dc.prado@gmail.com>
2025-07-15 22:46:30 -0300

    Agendamento de Consultas

---
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,33 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("consultas")
+@AllArgsConstructor
+public class ConsultaController {
+
+    private AgendaDeConsultas agenda;
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        agenda.agendar(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agenda.cancelar(dados);
+        return ResponseEntity.noContent().build();
+    }
+}
