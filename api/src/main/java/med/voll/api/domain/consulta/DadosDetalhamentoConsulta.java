78c291d24688bf12915c09ca27cef1b64c006da2
Douglas Prado <dc.prado@gmail.com>
2025-07-17 21:47:14 -0300

    Regras de negócio

--- api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ api/src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -9,4 +9,7 @@ public record DadosDetalhamentoConsulta(
         Long idPaciente,
         LocalDateTime data
 ) {
+    public DadosDetalhamentoConsulta(Consulta consulta) {
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+    }
 }
