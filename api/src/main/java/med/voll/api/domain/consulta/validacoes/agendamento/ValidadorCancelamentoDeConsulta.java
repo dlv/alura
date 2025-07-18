642d9f6081c8bf8ab67d950f83e825a26c19c80f
Douglas Prado <dc.prado@gmail.com>
2025-07-18 04:39:39 -0300

    Regras de negócio - v2

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorCancelamentoDeConsulta.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+
+public interface ValidadorCancelamentoDeConsulta {
+
+    void validar(DadosCancelamentoConsulta dados);
+}
