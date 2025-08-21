799a1d6eb84e3695fb227eaa0f8be42cc06797ab
Douglas Prado <dc.prado@gmail.com>
2025-08-21 11:59:56 -0300

    refatorando pastas

--- api/src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorCancelamentoDeConsulta.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.domain.consulta.validacoes.agendamento;
-
-import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
-
-public interface ValidadorCancelamentoDeConsulta {
-
-    void validar(DadosCancelamentoConsulta dados);
-}
